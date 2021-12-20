5d1ed26e603e93f72f6bfc4bc2bdaa23aa94a356
alimukhtor <james1999007@gmail.com>
2021-12-20 12:47:52 +0100

    created Planner table

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import { testConnection, connectDB } from "./db/connect.js";
+import plannerRouter from './db/services/planner/index.js'
+
+
+
 
 const server = express();
 
 const PORT = process.env.PORT || 3001;
+
+import Planner from './db/models/planner.js'
+// ***************************** TABLE RELATIONS *****************
+// Planner.hasMany(Task, { onDelete: "CASCADE" });
+// Task.belongsTo(Planner, { onDelete: "CASCADE" });
+
+export {Planner}
+
+// ***************************** END OF TABLE RELATIONS *****************
+
+
+// ************************* START OF ROUTERS **************************
+  server.use(express.json())
+  server.use("/planners", plannerRouter)
+
+  // ************************* END OF ROUTERS **************************
+
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
